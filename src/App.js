ff554485f47cfcb634cd3df88108875c7dc67fff
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-06-25 14:32:02 -0600

    nav and base styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Game from "../src/pages/Game";
+import GameOver from "../src/pages/GameOver";
+import Home from "../src/pages/Home";
+import HighScores from "../src/pages/HighScores";
+import NavBar from "../src/components/NavBar";
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
   return (
-    <div>
-      <h1> app </h1>
-    </div>
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+          <NavBar />
+          <Switch>
+            {/*   You have the home path last when using the switch so if all other routes fail will send them to final route which is home   */}
+            <Route path="/game" component={Game} />
+            <Route path="/highscores" component={HighScores} />
+            <Route path="/gameover" component={GameOver} />
+            <Route path="/home" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </Router>
   );
 }
 
---
+++ src/pages/Game.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Game() {
+  return (
+    <div>
+      <h1>Game Page</h1>
+    </div>
+  );
+}
