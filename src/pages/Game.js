5ac8db13b9e0b288084dac1682902510ebcc1056
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-06-26 17:36:43 -0600

    played with basic hooks for score and timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyledGame,
   StyledScore,
@@ -8,10 +8,21 @@ import {
 import { Strong } from "../styled/RandomStyles";
 
 export default function Game() {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const Interval = setInterval(() => {
+      setScore((previousScore) => previousScore + 1);
+    }, 1000);
+    return () => {
+      clearInterval(Interval);
+    };
+  }, [score]);
+
   return (
     <StyledGame>
       <StyledScore>
-        Score:<Strong>4</Strong>
+        Score:<Strong>{score}</Strong>
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
