c669b3f1a22e173e5adec7dc90ceaff15535b650
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-06-26 17:23:15 -0600

    styled game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+  StyledCharacter,
+} from "../styled/Game";
+import { Strong } from "../styled/RandomStyles";
 
 export default function Game() {
   return (
-    <div>
-      <h1>Game Page</h1>
-    </div>
+    <StyledGame>
+      <StyledScore>
+        Score:<Strong>4</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Timer: <Strong>00: 000</Strong>
+      </StyledTimer>
+    </StyledGame>
   );
 }
