20169a752bc43e01d200ce3f4ce72a1343044313
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-07-01 14:56:39 -0600

    implimented the timer with hooks

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,17 +7,43 @@ import {
 } from "../styled/Game";
 import { Strong } from "../styled/RandomStyles";
 
-export default function Game() {
+export default function Game({ history }) {
   const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(999);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
-    const Interval = setInterval(() => {
-      setScore((previousScore) => previousScore + 1);
-    }, 1000);
-    return () => {
-      clearInterval(Interval);
-    };
-  }, [score]);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMsStr = ("0000" + msPassedStr).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMsStr.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 - parseInt(formattedMsStr.substring(formattedMsStr.length - 3));
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameover");
+    }
+  }, [seconds, ms, history]);
 
   return (
     <StyledGame>
@@ -26,7 +52,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Timer: <Strong>00: 000</Strong>
+        Timer:{" "}
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
