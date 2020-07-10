59be822dfa3eb6ab0e2a12b67e73cdfbd8b54bd0
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-07-10 08:52:23 -0600

    checking in env variables

--- functions/getHighScore.js
+++ functions/getHighScore.js
@@ -1,16 +1,31 @@
+require("dotenv").config();
+
 const Airtable = require("airtable");
 
 Airtable.configure({
-  apiKey: "keyC0L1DoS6vboLIV",
+  apiKey: process.env.AIRTABLE_API_KEY,
 });
 
-const base = require("airtable").base("appga1Hx5KtN8YLK6");
-const table = base.table("Table");
+const base = require("airtable").base(process.env.AIRTABLE_BASE);
+const table = base.table(process.env.AIRTABLE_TABLE);
 
 exports.handler = async (event) => {
-  const records = table.select({}).firstPage();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(records),
-  };
+  // return all of records from first page of table instead of max amount of records
+  // try catch is used for async in case an error does happen something else returns
+  try {
+    const records = table.select().firstPage();
+    const formattedRecords = records.map((record) => ({
+      id: record.id,
+      fields: record.fields,
+    }));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(formattedRecords),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: "failed to query from table" }),
+    };
+  }
 };
